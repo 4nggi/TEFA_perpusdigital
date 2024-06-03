904c2f945ef6dadef1fee034cfcc021164ae7903
4nggi <148174181+4nggi@users.noreply.github.com>
2024-06-03 10:07:59 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,42 +1,21 @@
 <template>
-  <div class="row p-3 bg-primary text-white">
-    <div class="col-lg-1">
-    <img src="~/assets/img/LOGO-SMK4.png" alt="logo SMKN 4" height="100px" width="100px">
-  </div>
-
-  <div class="col-lg-11">
-    <h1>Perpus Digital</h1>
-    <address>
-      SMKN 4 Tasikmalaya
-      Jl.Depok, Sukamenak, Purbaratu
-      </address>
-    </div>
+  <div class="row p-3 color text-white">
+      <div class="col-lg-1">
+          <img src="~/assets/img/LOGO-SMK4.png" alt="logo smkn4" height="100px" width="100px">
+      </div>
+      <div class="col-lg-11">
+          <h1>PERPUS DIGITAL</h1>
+          <address>
+              SMKN 4 Tasikmalaya
+          </address>
+          <address>
+              JL.Depok,Sukamanah,Purbaratu
+          </address>
+      </div>
   </div>
 </template>
-
-<style scoped>
-.header {
-  display: flex;
-  align-items: center;
-  width: 30%;
-  height: 30px;
-  background-color: 0075FF;
-  color: #fff;
-}
-.h-img {
-  margin-left: 25px;
-}
-.h-img > img{
-  width: 100px;
-}
-.h-text {
-  margin-left: 3px;
-  line-height: 10px;
-;
-}
-.t-judul {
-  margin-left: 2px;
-  font-size: 3px;
-  font-weight: 500;
+<style>
+.color{
+  background-color:  #031e92;
 }
 </style>
--- components/statistik.vue
+++ components/statistik.vue
@@ -1,49 +1,45 @@
- <template>
+<template>
   <div>
-      <canvas id="myChart"></canvas>
+    <canvas id="statistik"></canvas>
   </div>
- </template>
-
- <script setup>
-import Chart from 'Chart.js/auto';
-
-const labels=[
-  'January',
-  'February',
-  'March',
-  'April',
-  'Mei',
-  'june',
-  'july'
-];
-
-const data= {
-  labels: labels,
-  datasets: [{
-    labels: 'pengunjung',
-    backgroundcolor: [
-    'rgb(20, 112, 164)',
-    'rgb(40, 83, 62)',
-    'rgb(46, 61, 139)',
-    'rgb(255, 235, 51)',
-    'rgb(216, 57, 57)',
-    'rgb(158, 90, 94)',
-    'rgb(216, 57, 57)'
-    ],
-    borderColor: 'rgb(255, 99, 132)',
-    data: [30, 10, 10, 20, 30, 5, 10],
-}]
-};
-
-const config = {
-  type: 'bar',
-  data: data,
-  options: {}
-};
-onMounted(()=>{
-  const myChart = new Chart(
-  document.getElementById('myChart'),
-  config
-);
-})
-</script>
+  </template>
+  
+  <script setup>
+  import Chart from 'chart.js/auto'
+  
+  const labels = [
+    'january',
+    'february',
+    'march',
+    'april',
+    'mei',
+    'june',
+  ];
+  const data ={
+    labels:labels,
+    datasets:[{
+      label: 'pengunjung',
+      data: [6, 8, 10, 9, 9,3],
+      backgroundColor:[
+        'rgb(217, 217, 217)',
+        'rgb(217, 217, 217)',
+        'rgb(217, 217, 217)',
+        'rgb(217, 217, 217)',
+        'rgb(217, 217, 217)',
+        'rgb(217, 217, 217)'
+      ],
+    }]
+  };
+  const  config ={
+    type: 'bar',
+    data: data,
+    options: {}
+  };
+  onMounted(()=>{
+    const myChart = new Chart(
+      document.getElementById('statistik'),
+      config
+    );
+  })
+  
+  </script>
