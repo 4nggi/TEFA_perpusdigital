23f75187fbbfc9143c28c1e31aa5cb665b937e82
4nggi <148174181+4nggi@users.noreply.github.com>
2024-06-03 09:14:05 +0700

    Add files via upload

---
+++ components/statistik.vue
@@ -0,0 +1,49 @@
+ <template>
+  <div>
+      <canvas id="myChart"></canvas>
+  </div>
+ </template>
+
+ <script setup>
+import Chart from 'Chart.js/auto'
+
+const labels=[
+  'January',
+  'February',
+  'March',
+  'April',
+  'Mei',
+  'june',
+  'july'
+];
+
+const data= {
+  labels: labels,
+  datasets: [{
+    labels: 'pengunjung',
+    backgroundcolor: [
+    'rgb(20, 112, 164)',
+    'rgb(40, 83, 62)',
+    'rgb(46, 61, 139)',
+    'rgb(255, 235, 51)',
+    'rgb(216, 57, 57)',
+    'rgb(158, 90, 94)',
+    'rgb(216, 57, 57)'
+    ],
+    borderColor: 'rgb(255, 99, 132)',
+    data: [30, 10, 10, 20, 30, 5, 10],
+}]
+};
+
+const config = {
+  type: 'bar',
+  data: data,
+  options: {}
+};
+onMounted(()=>{
+  const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
+})
+</script>
---
+++ pages/Rincian/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container shadow mt-5 p-5">
+    <div class="row mt-4">
+      <div class="col-4">
+        <h2>Rincian Buku</h2>
+        <img src="~/assets/img/deatnote.png" class="Buku" alt="Buku1" height="350px" width="300px" />
+      </div>
+      <div class="col-4 mt-5">
+        <br />
+        <p>Judul Buku : Death Note</p>
+        <p>No Induk : 22.00753.1</p>
+        <p>Penulis : Ryuk</p>
+        <p>Tahun terbit : 2007</p>
+        <p>Penerbit : shinigami</p>
+        <p>Kategori : Novel</p>
+        <p>Rak : 2</p>
+      </div>
+    </div>
+    <div class="row">
+      <p class="mt-3">  buku milik Shinigami (Dewa Kematian), yang memiliki kekuatan supranatural di mana buku tersebut dapat membunuh siapa saja dengan menuliskan nama korbannya.</p>
+    </div>
+  </div>
+</template>
+<style scoped></style>
